87bb1a777c6982548e4152429bb5d5916def9d7f
Autumnal_Joy <autumnal_joy@foxmail.com>
2021-07-04 15:05:38 +0800

    PC同步

--- nopipe/HDCPU.v
+++ nopipe/HDCPU.v
@@ -3,7 +3,7 @@ module HDCPU(
     input T3,
     input C,
     input Z,
-    input[2:0] SW,
+    input [2:0] SW,
     input [7:4] IR,
     input [3:1] W,
     output reg LDC,
@@ -33,11 +33,16 @@ module HDCPU(
     reg SST0 = 0;
     reg [2:0] flag = 0;
     reg EI = 1;
-    reg [7:0] PC = 0;
+    reg [7:0] SELF_PC = 0;
+    reg [2:0] count;
+    reg [7:0] SELF_R0;
+    reg [7:0] SELF_IR;
 
-    always @(negedge T3 or negedge CLR)
+    always @(negedge T3, negedge CLR)
     begin
-        if (!CLR) ST0 <= 0;
+        if (!CLR) begin
+            ST0 <= 0;
+        end
         else if (!T3) begin
             if (SST0 == 1'b1)
                 ST0 = SST0; // 有SST0 == 1就立刻ST0    = 1
@@ -46,14 +51,14 @@ module HDCPU(
         end
     end
 
-
-    always @(SW or W or CLR or IR) // ?是否需要把T3专门写成脉冲形式
+    always @(SW, W, CLR, IR) // ?是否需要把T3专门写成脉冲形式
     begin
         {LDC, LDZ, CIN, M, ABUS, DRW, PCINC, LPC, LAR, PCADD, ARINC, SELCTL, MEMW, STOP, LIR, SBUS, MBUS, SHORT, LONG, S, SEL} = 0;
 
-        if (CLR == 0) begin
+        if (!CLR) begin
             SST0 <= 0;
-            PC <= 0;
+            count <= 0;
+            SELF_PC <= 0;
             EI <= 1;
         end
         else begin
@@ -69,11 +74,11 @@ module HDCPU(
                     SST0   <= W[1];
                 end
                 3'b010: begin
-                    SBUS   <= W[1]&&!ST0;
-                    LAR    <= W[1]&&!ST0;
-                    SST0   <= W[1]&&!ST0;
-                    MBUS   <= W[1]&&ST0;
-                    ARINC  <= W[1]&&ST0;
+                    SBUS   <= W[1] && !ST0;
+                    LAR    <= W[1] && !ST0;
+                    SST0   <= W[1] && !ST0;
+                    MBUS   <= W[1] && ST0;
+                    ARINC  <= W[1] && ST0;
                     STOP   <= W[1];
                     SHORT  <= W[1];
                     SELCTL <= W[1];
@@ -87,7 +92,6 @@ module HDCPU(
                     SEL[0] <= W[1] || W[2];
                 end
                 3'b100: begin
-
                     SBUS   <= W[1] || W[2];
                     SELCTL <= W[1] || W[2];
                     DRW    <= W[1] || W[2];
@@ -106,7 +110,7 @@ module HDCPU(
                             LIR   = W[1];
                             PCINC = W[1];
                             if (EI)
-                                PC = PC + 1;
+                                SELF_PC = SELF_PC + 1;
                             case (IR)
                                 4'b0001: begin // ADD
                                     S    = 4'b1001;
@@ -160,14 +164,14 @@ module HDCPU(
                                         if(EI) begin
                                             LONG = W[2];
                                             ABUS = W[3];
-                                            SEL3 = !W[3];
-                                            SEL2 = !W[3];
+                                            SEL[3] = !W[3];
+                                            SEL[2] = !W[3];
                                             S = 1100;
-                                            SELCTL = W[3]
-                                            CIN = W[3]
-                                            DRW = W[3]
-                                            LDZ = W[3]
-                                            LDC = W[3]
+                                            SELCTL = W[3];
+                                            CIN = W[3];
+                                            DRW = W[3];
+                                            LDZ = W[3];
+                                            LDC = W[3];
                                             flag = 1;
                                         end
                                     end
@@ -177,14 +181,14 @@ module HDCPU(
                                         if(EI) begin
                                             LONG = W[2];
                                             ABUS = W[3];
-                                            SEL3 = !W[3];
-                                            SEL2 = !W[3];
+                                            SEL[3] = !W[3];
+                                            SEL[2] = !W[3];
                                             S = 1100;
-                                            SELCTL = W[3]
-                                            CIN = W[3]
-                                            DRW = W[3]
-                                            LDZ = W[3]
-                                            LDC = W[3]
+                                            SELCTL = W[3];
+                                            CIN = W[3];
+                                            DRW = W[3];
+                                            LDZ = W[3];
+                                            LDC = W[3];
                                             flag = 1;
                                         end
                                     end
@@ -193,7 +197,7 @@ module HDCPU(
                                     S    = 4'b1111;
                                     ABUS = W[2];
                                     LPC  = W[2];
-                                    // TODO: PC同步
+                                    // TODO: SELF_PC同步
                                 end
                                 // 额外指令
                                 4'b1010: begin // OUT
@@ -226,21 +230,172 @@ module HDCPU(
                             endcase
                             // <---SW == 000的情况执行完毕
                         end
-                        // 将R0寄存器提取并保存至软件
+                        // 将SELF_R0寄存器提取并保存至软件
                         3'b001: begin
+                            SELF_R0 = SELF_R0 << 1;
+                            SELF_R0 = SELF_R0 + C;
+                            SHORT = W[1];
+                            if (count < 7) begin
+                                count = count + 1;
+                                ABUS = W[1];
+                                SEL[3] = !W[1];
+                                SEL[2] = !W[1];
+                                S = 1100;
+                                SELCTL = W[1];
+                                CIN = W[1];
+                                DRW = W[1];
+                                LDZ = W[1];
+                                LDC = W[1];
+                            end
+                            else begin
+                                flag = 2;
+                                count = 0;
+                            end
                         end
-                        // 将PC提取至R0寄存器
+                        // 将SELF_PC提取至SELF_R0寄存器
                         3'b010: begin
+                            if (count < 8) begin
+                                count = count + 1;
+                                ABUS = W[1];
+                                SEL[3] = !W[1];
+                                SEL[2] = !W[1];
+                                SELCTL = W[1];
+                                S = 1100;
+                                CIN = W[1];
+                                DRW = W[1];
+                                LDZ = W[1];
+                                LDC = W[1];
+                                if (SELF_PC[8-count]) begin
+                                    SELCTL = W[2];
+                                    SEL[3] = !W[2];
+                                    SEL[2] = !W[2];
+                                    S = 0000;
+                                    ABUS = W[2];
+                                    DRW = W[2];
+                                    LDZ = W[2];
+                                    LDC = W[2];
+                                end
+                                else
+                                    SHORT = W[1];
+                            end
+                            else begin
+                                count = 0;
+                                SELCTL = 1;
+                                SEL[3] = 0;
+                                SEL[2] = 0;
+                                SEL[1] = 0;
+                                SEL[0] = 0;
+                                M = W[1];
+                                S = W[1] ? 1010 : 1100;
+                                ABUS = W[1] || W[3];
+                                LAR = W[1];
+                                MBUS = W[2];
+                                DRW = W[2] || W[3];
+                                LONG = W[2];
+                                CIN = W[3];
+                                LDZ = W[3];
+                                LDC = W[3];
+                                if (W[3])
+                                    flag = 3;
+                            end
                         end
-                        // 将R0寄存器提取至软件
+                        // 将SELF_R0寄存器提取至软件
                         3'b011: begin
+                            SELF_IR = SELF_IR << 1;
+                            SELF_IR = SELF_IR + C;
+                            SHORT = W[1];
+                            if (count < 7) begin
+                                count = count + 1;
+                                ABUS = W[1];
+                                SEL[3] = !W[1];
+                                SEL[2] = !W[1];
+                                S = 1100;
+                                SELCTL = W[1];
+                                CIN = W[1];
+                                DRW = W[1];
+                                LDZ = W[1];
+                                LDC = W[1];
+                            end
+                            else begin
+                                count = 0;
+                                flag = 4;
+                                SELF_PC = SELF_PC + SELF_IR & 2'h0f;
+                                if (SELF_IR >= 8)
+                                    SELF_PC = SELF_PC - 16;
+                            end
                         end
-                        // 恢复R0初始值
+                        // 恢复SELF_R0初始值
                         3'b100: begin
+                            if (count < 8) begin
+                                count = count + 1;
+                                ABUS = W[1];
+                                SEL[3] = !W[1];
+                                SEL[2] = !W[1];
+                                SELCTL = W[1];
+                                S = 1100;
+                                CIN = W[1];
+                                DRW = W[1];
+                                LDZ = W[1];
+                                LDC = W[1];
+                                if (SELF_R0[8-count]) begin
+                                    SELCTL = W[2];
+                                    SEL[3] = !W[2];
+                                    SEL[2] = !W[2];
+                                    S = 0000;
+                                    ABUS = W[2];
+                                    DRW = W[2];
+                                    LDZ = W[2];
+                                    LDC = W[2];
+                                end
+                                else
+                                    SHORT = W[1];
+                            end
+                            else begin
+                                count = 0;
+                                SHORT = W[1];
+                                flag = 0;
+                            end
                         end
-                        // 恢复PC初始值
+                        // 恢复SELF_PC初始值
                         3'b101: begin
+                            if (count < 8) begin
+                                count = count + 1;
+                                ABUS = W[1];
+                                SEL[3] = !W[1];
+                                SEL[2] = !W[1];
+                                SELCTL = W[1];
+                                S = 1100;
+                                CIN = W[1];
+                                DRW = W[1];
+                                LDZ = W[1];
+                                LDC = W[1];
+                                if (SELF_PC[8-count]) begin
+                                    SELCTL = W[2];
+                                    SEL[3] = !W[2];
+                                    SEL[2] = !W[2];
+                                    S = 0000;
+                                    ABUS = W[2];
+                                    DRW = W[2];
+                                    LDZ = W[2];
+                                    LDC = W[2];
+                                end
+                                else
+                                    SHORT = W[1];
+                            end
+                            else begin
+                                count = 0;
+                                SHORT = W[1];
+                                flag = 0;
+                                SELCTL = W[1];
+                                SEL[1] = 0;
+                                SEL[0] = 0;
+                                M = 1;
+                                S = 1010;
+                                ABUS = 1;
+                                LPC = 1;
+                            end
                         end
+                    endcase
                 end
                 // default:
             endcase
